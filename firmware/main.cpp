0397118abb030b633cf10d17cbcd8de0a5c8fea0
Teisybe <teisybeltu@gmail.com>
2023-08-03 23:21:58 +0300

    Firmware: Set configuration fuses
    Configuration fuses were added to the `main.cpp` file. Currently, the
    MCU is configured to run at the speed of 8 MHz using an internal RC
    oscillator. All other fuses are set to their default values outlined in
    the datasheet.

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -1,4 +1,16 @@
 
+#include <avr/fuse.h>
+
+FUSES =
+{
+    // Calibrated internal RC oscillator, default start up time
+    .low = FUSE_CKSEL0 & FUSE_CKSEL2 & FUSE_CKSEL3 & FUSE_SUT0,
+
+    // Defaults
+    .high = HFUSE_DEFAULT,
+    .extended = EFUSE_DEFAULT 
+};
+
 int main()
 {
     while(true)
